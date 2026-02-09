d4c54225e4dda501d5d53c8b4563fe2998022ece
Vergos1 <legendsmobajl4@gmail.com>
2026-02-09 19:50:48 +0200

    update: add initial configuration files and component structure

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import next from 'eslint-config-next';
+
+const eslintConfig = [
+	{
+		ignores: ['.next/**', 'node_modules/**'],
+	},
+	...next,
+	...pluginQuery.configs['flat/recommended'],
+	{
+		files: ['**/*.{ts,tsx}'],
+		languageOptions: {
+			parser: tsParser,
+			parserOptions: {
+				ecmaVersion: 'latest',
+				sourceType: 'module',
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+		},
+		plugins: {
+			'@typescript-eslint': tsPlugin,
+			'simple-import-sort': simpleImportSort,
+		},
+		rules: {
+			'@typescript-eslint/no-unused-vars': 'off',
+			'simple-import-sort/imports': 'error',
+			'simple-import-sort/exports': 'error',
+		},
+	},
+];
+
+export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	experimental: {
+		optimizePackageImports: ['@tanstack/react-query', 'zustand'],
+	},
 };
 
 export default nextConfig;
