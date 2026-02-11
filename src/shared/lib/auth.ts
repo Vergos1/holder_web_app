46c5625dfdfca5fd584849a796d4de79ecae8f4c
Vergos1 <legendsmobajl4@gmail.com>
2026-02-11 14:57:18 +0200

    feat: implement authentication routes using better-auth and update auth configuration

--- src/shared/lib/auth.ts
+++ src/shared/lib/auth.ts
@@ -1,5 +1,15 @@
 import { betterAuth } from 'better-auth';
 
 export const auth = betterAuth({
-	//...
+	emailAndPassword: { enabled: false },
+	socialProviders: {
+		google: {
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		},
+		facebook: {
+			clientId: process.env.FACEBOOK_CLIENT_ID!,
+			clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+		},
+	},
 });
