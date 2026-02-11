f76fc8f3f792acb81b678a4d76c272773b240332
Vergos1 <legendsmobajl4@gmail.com>
2026-02-11 15:37:58 +0200

    feat: update proxy middleware to use session cookies for authentication and enhance route matching

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,16 @@
-import { getSession } from '@shared-lib';
+import { getSessionCookie } from 'better-auth/cookies';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function proxy(request: NextRequest) {
-	const session = await getSession();
-	if (!session) return NextResponse.redirect(new URL('/sign-in', request.url));
+	const sessionCookie = getSessionCookie(request);
+
+	if (!sessionCookie) {
+		return NextResponse.redirect(new URL('/sign-in', request.url));
+	}
+
 	return NextResponse.next();
 }
 
 export const config = {
-	matcher: ['/profile'],
+	matcher: ['/profile/:path*'],
 };
